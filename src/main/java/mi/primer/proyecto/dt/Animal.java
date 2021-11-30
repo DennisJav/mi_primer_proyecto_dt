e5f082b503d84e884cd976bb8dc30f449d82c78b
Dennis <Dennis@DESKTOP-D84QG4M>
2021-11-29 22:58:04 -0500

    Primera

---
+++ src/main/java/mi/primer/proyecto/dt/Animal.java
@@ -0,0 +1,10 @@
+package mi.primer.proyecto.dt;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		System.out.println("PRIMERO");
+	}
+
+}
