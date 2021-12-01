3ca86b90a6365f852a450a66c4900bf74bf23a50
Dennis <Dennis@DESKTOP-D84QG4M>
2021-11-30 20:17:41 -0500

    Instalacion de Maven

--- src/main/java/mi/primer/proyecto/dt/Animal.java
+++ src/main/java/mi/primer/proyecto/dt/Animal.java
@@ -4,8 +4,15 @@ public class Animal {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
+		int edad = 25;
+		int edadMadre = 30;
+		int edadTotal = edad+edadMadre;
+		
+		String cedula = "222";
+			
 		System.out.println("PRIMERO");
 		System.out.println("Segundo cambio");
+		
 	}
 
 }
---
+++ src/main/java/mi/primer/proyecto/dt/PruebaMaven.java
@@ -0,0 +1,8 @@
+package mi.primer.proyecto.dt;
+
+public class PruebaMaven {
+	public static void main(String[] args) {
+		// TODO Esbozo de método generado automáticamente
+		System.out.println("proyecto maven");
+	}
+}
