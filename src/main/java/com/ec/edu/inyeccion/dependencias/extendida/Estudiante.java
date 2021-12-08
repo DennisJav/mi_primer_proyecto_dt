48de50843497ac63bec646bd3a791f813023f57b
Dennis <Dennis@DESKTOP-D84QG4M>
2021-12-07 20:50:21 -0500

    inyeccion Dependencias

--- src/main/java/com/ec/edu/inyeccion/dependencias/extendida/Estudiante.java
+++ src/main/java/com/ec/edu/inyeccion/dependencias/extendida/Estudiante.java
@@ -7,7 +7,15 @@ public class Estudiante {
 	protected String apellido;
 	protected Direccion direccion;
 	
-
+	
+	public void pagarServipagos(String Nombre) {
+			
+		System.out.println(Nombre+" El estudiante hace el pago");
+		System.out.println(Nombre + " El estudiante presenta el pago");
+		System.out.println("efectvo");
+	
+	}
+	
 	//metodos set y get
 	
 	public String getNombre() {
