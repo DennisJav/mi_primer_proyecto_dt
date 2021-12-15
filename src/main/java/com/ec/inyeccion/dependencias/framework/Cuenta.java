87952d1201ef8fbdbec50865d9f93b54836fcf6b
Dennis <Dennis@DESKTOP-D84QG4M>
2021-12-14 20:50:11 -0500

    Taller parte 2 indiv

--- src/main/java/com/ec/inyeccion/dependencias/framework/Cuenta.java
+++ src/main/java/com/ec/inyeccion/dependencias/framework/Cuenta.java
@@ -2,4 +2,36 @@ package com.ec.inyeccion.dependencias.framework;
 
 public class Cuenta {
 
+	private String numero;
+	private int saldo;
+	
+	
+	public Cuenta() {
+		
+	}
+	
+	public Cuenta(String numero, int saldo) {
+		super();
+		this.numero = numero;
+		this.saldo = saldo;
+	}
+	
+	
+	//metodos set y get
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public int getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+	
+	
+	
+
 }
--- src/main/java/com/ec/inyeccion/dependencias/framework/Main.java
+++ src/main/java/com/ec/inyeccion/dependencias/framework/Main.java
@@ -1,27 +1,38 @@
 package com.ec.inyeccion.dependencias.framework;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
 
-	private String numero;
-	private int saldo;
+	public static void main(String[] args) {
+
+		ApplicationContext app = new ClassPathXmlApplicationContext("com/uce/edu/archivo/beans.xml");
+		Cuenta miCuenta = (Cuenta) app.getBean("cuenta");
+		System.out.println(miCuenta.getNumero());
+		System.out.println(miCuenta.getSaldo());
+
+//		Codigo Acoplado
+//  	
+//		Cuenta miCuentaPrueba=new Cuenta();
+//		miCuentaPrueba.setNumero("25");
+//		miCuentaPrueba.setSaldo(156);
+//		
+//		Cuenta miCuentaPrueba2=new Cuenta("1234",156);
+//		
+
+		Cuenta miCuenta1 = (Cuenta) app.getBean("cuenta1");
+		System.out.println(miCuenta1.getNumero());
+		System.out.println(miCuenta1.getSaldo());
+		
+		
+//		IoC por constructor
+		Estudiante estud =  (Estudiante)app.getBean("estudiante");
+		int edadtotal = estud.getEdad()+10;
+		System.out.println(estud.getNombre()+" "+estud.getApellido()+" "+edadtotal);
+		
+		
 	
 	
-	
-	
-	//metodos set y get
-	
-	public String getNumero() {
-		return numero;
 	}
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-	public int getSaldo() {
-		return saldo;
-	}
-	public void setSaldo(int saldo) {
-		this.saldo = saldo;
-	}
-	
-	
 }
